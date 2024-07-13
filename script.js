42e3b66fec74804750f09762ee77f999d64dbcac
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-07-13 20:56:05 +0200

    first version of the todo list
    - search function has to be implemented

---
+++ script.js
@@ -0,0 +1,29 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+
+const generateTemplate  = (todo) => {
+    const html =  `<li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>`;
+    list.innerHTML += html;
+}
+
+addForm.addEventListener("submit" , e  => {
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+    if(todo.length){
+        generateTemplate(todo);
+        addForm.reset();
+    }
+
+});
+
+//delete todos
+list.addEventListener("click", e => {
+// check if we clicked the trashcan
+    if(e.target.classList.contains("delete")){
+        e.target.parentElement.remove();
+    }   
+});
